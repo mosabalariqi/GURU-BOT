70c2ff6299556da69f6b68f824fb93e9c1a58e32
mosabalariqi <131707455+mosabalariqi@users.noreply.github.com>
2023-06-09 19:27:36 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['967737427652', 'mosabalariqi', true],
+  ['967771451738', 'mosab', true], 
   [''] 
 ] //Number of owners
 
